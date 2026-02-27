32ebfd31bac021fb5e83be7e6dd0a2a145687e97
blob40 <alexandrialax40@gmail.com>
2026-02-27 17:24:23 +0000

    starting spot

--- HashTable.java
+++ HashTable.java
@@ -2,73 +2,93 @@ import java.util.*;
 
 import javax.swing.text.AttributeSet;
 import javax.swing.text.html.HTML.Tag;
-import javax.swing.text.html.HTMLDocument.Iterator;
-
 
 import java.io.*;
 
 public class HashTable {
 
-    int capacity = 128;
-    float loadFactor = .75f;
-    private LinkedList <String> [] buckets;
+    private int size;
+    private LinkedList<String>[] buckets;
+
+    public HashTable() {
+        buckets = new LinkedList[100];
+    }
 
     // Where collision setup has to be?
-    //Chaining create a linked list withing each index spot
+    // Chaining create a linked list withing each index spot
     public void put(String key) {
         int hashCode = key.hashCode();
-        int arrayIndex = Math.abs(hashCode) % capacity;
-     
-       
-        if (buckets[arrayIndex] == null){
+        int arrayIndex = Math.abs(hashCode) % buckets.length;
+
+        if (buckets[arrayIndex] == null) {
             buckets[arrayIndex] = new LinkedList<String>();
-        } 
-            buckets[arrayIndex].add(key);
-        
-       
-
-        if (capacity > ((capacity*2)/3)){
-            capacity*=2;
-        
-            int newHashCode = key.hashCode();
-            int newArrayIndex = Math.abs(newHashCode) % capacity;
-
-        if (buckets[newArrayIndex] != null && buckets[newArrayIndex].contains(key)) {
-            // Key already exists in new bucket
-        } else {
-            buckets[newArrayIndex].add(key);
         }
-            //use iterator to reput things in
+        buckets[arrayIndex].add(key);
+        size++;
+
+        if (size > ((buckets.length * 2) / 3)) {
+
+            // take everything and re add it to a new table that's twice as big
+            LinkedList<String>[] temp = buckets;
+            buckets = new LinkedList[buckets.length * 2];
+            // loop through all the temp elements
+            // loop through the inner lists
+            // call put on all of them
+
+            for (int i = 0; i < temp.length; i++) {
+                if (temp[i] != null) {
+                    if (buckets[arrayIndex] == null) {
+                        buckets[arrayIndex] = new LinkedList<String>();
+                    }
+                    buckets[arrayIndex].add(key);
+                }
+            }
         }
     }
 
     // Has to think about collisions here
     public String get(String key) {
         int hashCode = key.hashCode();
-        int arrayIndex = Math.abs(hashCode) % capacity;
+        int arrayIndex = Math.abs(hashCode) % buckets.length;
 
         if (buckets[arrayIndex] != null) {
-            return buckets[arrayIndex].get(0); // Return first element in bucket
+            int index = 0;
+            while (index < buckets[arrayIndex].size() && !buckets[arrayIndex].get(index).equals(key)) {
+                index++;
+            }
+            if (buckets[arrayIndex].get(index).equals(key)) {
+                return "found";
+            } else {
+                return "not found";
+            }
         }
         return null;
     }
 
     public Integer remove(String key) {
         int hashCode = key.hashCode();
-        int arrayIndex = Math.abs(hashCode) % capacity;
+        int arrayIndex = Math.abs(hashCode) % buckets.length;
 
         if (buckets[arrayIndex] != null && buckets[arrayIndex].contains(key)) {
             buckets[arrayIndex].remove(key);
-            return 1; 
+            return 1;
         }
-        return 0; 
+        return 0;
     }
 
     public Iterator keys() {
-        return null;
+        return new MyIterator();
     }
 
     public void print() {
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] != null) {
+                for (int x = 0; x < buckets[i].size(); x++) {
+                    String key = buckets[i].get(x);
+                    System.out.println(key);
+                }
+            }
+        }
     }
 
     /**
@@ -123,7 +143,7 @@ public class HashTable {
     public void save() {
         FileOutputStream stream;
         PrintWriter printWriter = null;
-        Iterator iterator;
+        Iterator<String> iterator;
 
         // Open the file for writing
         try {
@@ -136,10 +156,10 @@ public class HashTable {
 
         // Write the contents of this HashTable to the file
         iterator = keys();
-        while (((HashTable.MyIterator) iterator).hasNext()) {
-            String key = (String) iterator.next();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
             printWriter.println(key);
-            String value = (String) get(key);
+            String value = get(key);
             value = removeNewlines(value);
             printWriter.println(value);
             printWriter.println();
@@ -160,55 +180,31 @@ public class HashTable {
         return value.replaceAll("\r|\n", " ");
     }
 
-    private class MyIterator extends Iterator {
-    
-         @Override
-        public void next() {
-            if (hasNext()){
-                return table.keys().nextElement();
-            }  else {
-                throw new NoSuchElementException();
-            }
+    private class MyIterator implements Iterator {
+        int index = 0;
+
+        @Override
+        public String next() {
+            if (hasNext()) {
+                for (int i = 0; i < buckets.length; i++) {
+                    index++;
+                    if (buckets[i] != null) {
+                        return buckets[i].get(index);
+                    }
+                }
+            } 
+            return null;
         }
 
-         public boolean hasNext() {
-            if (table.size() > 0){
-                return true;
+        public boolean hasNext() {
+            if (buckets != null) {
+                for (int i = 0; i < buckets.length; i++) {
+                    if (buckets[i] != null && buckets[i].size() > 0) {
+                        return true;
+                    }
+                }
             }
             return false;
         }
-
-
-        @Override
-        public AttributeSet getAttributes() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'getAttributes'");
-        }
-
-        @Override
-        public int getStartOffset() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'getStartOffset'");
-        }
-
-        @Override
-        public int getEndOffset() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'getEndOffset'");
-        }
-
-       
-        @Override
-        public boolean isValid() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'isValid'");
-        }
-
-        @Override
-        public Tag getTag() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'getTag'");
-        }
-
     }
 }
